8ec65e36dc70640eaad75170ed9057059e4c4b75
hloong <hbxglong@163.com>
2023-01-06 23:09:04 +0800

    新增一个vlayout的实例，vLayout已经废弃被Tangram项目替换了，这里主要是为了了解vLayout的源码学习

---
+++ app/src/main/java/com/hloong/interview/ui/custom/vlayout/VLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.hloong.interview.ui.custom.vlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hloong.interview.R
+
+class VLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_v_layout)
+    }
+}
