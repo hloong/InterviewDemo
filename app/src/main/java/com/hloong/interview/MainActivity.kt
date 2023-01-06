c9558bc6bbfe1510be54afff64e072b7375bf864
hloong <hbxglong@163.com>
2023-01-06 18:02:10 +0800

    增加一个SVG 地图点击 选中的实例

--- app/src/main/java/com/hloong/interview/MainActivity.kt
+++ app/src/main/java/com/hloong/interview/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hloong.interview.ui.car_ani.CarAniActivity
+import com.hloong.interview.ui.custom.mapview.MapChinaActivity
 import com.hloong.interview.ui.custom.recyclerview.CustomRvActivity
 import com.hloong.interview.ui.recyclerview.RecyclerViewActivity
 import com.hloong.interview.ui.splash_ani.SplashActivity
@@ -30,7 +31,9 @@ class MainActivity : AppCompatActivity() {
         btRvCustom.setOnClickListener {
             startActivity(Intent(this,CustomRvActivity::class.java))
         }
-
+        btSvgMap.setOnClickListener {
+            startActivity(Intent(this,MapChinaActivity::class.java))
+        }
     }
 
 
---
+++ app/src/main/java/com/hloong/interview/ui/custom/mapview/MapChinaActivity.kt
@@ -0,0 +1,12 @@
+package com.hloong.interview.ui.custom.mapview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hloong.interview.R
+
+class MapChinaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_china)
+    }
+}
